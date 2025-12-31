6501aaa93ae3e5c1889d83cf44e9ab4d282287d2
Cheryl Craig <cacraig@ucar.edu>
2025-12-31 10:54:55 -0700

    temporary workaround until get diags completed

---
+++ micro_pumas_tempfix.F90
@@ -0,0 +1,37 @@
+!CACNOTE - All outfld calls to history_out_field are converted
+         - All history_add_field calls are done
+               - Except the constituent history_add_field needs to be changed - see CACNOTE
+               - Need to replace "#" in units?
+         - need to line up calls
+!!!----------------------------------------------
+
+module micro_pumas_tempfix
+
+   use micro_pumas_diags, only: proc_rates_type
+
+   implicit none
+   private
+   save
+
+   public :: micro_pumas_tempfix_init ! init routine
+
+CONTAINS
+
+!> \section arg_table_micro_pumas_tempfix_init  Argument Table
+!! \htmlinclude micro_pumas_tempfix_init.html
+subroutine micro_pumas_tempfix_init(proc_rates, errmsg, errflg)
+
+   type (proc_rates_type), intent(out)  :: proc_rates
+
+
+   ! CCPP error handling variables
+   character(len=512), intent(out) :: errmsg
+   integer,            intent(out) :: errflg
+
+   errmsg = ''
+   errflg = 0
+
+
+end subroutine micro_pumas_tempfix_init
+
+end module micro_pumas_tempfix
